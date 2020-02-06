ff24cad074047b84b047deddbc3b7a80255889fd
Abel M. Osorio <abel.m.osorio@gmail.com>
2020-02-06 15:58:51 -0300

    Add a POST /sent route to forward emails to Sendgrid.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import axios from 'axios'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/send', async (req, res) => {
+  try {
+    axios.post('https://api.sendgrid.com/v3/mail/send', req.body, {
+      headers: {
+        Authorization: `Bearer ${process.env.VUE_APP_SENDGRID_API_KEY}`
+      }
+    })
+  } catch (err) {
+    res.status(500)
+    res.render('error', { error: err })
+  }
+
+  res.send('OK')
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
